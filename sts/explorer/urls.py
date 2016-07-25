8d708a16d2ac9278b673546821f35c753d8999a6
JD Godchaux <jd@nijel.org>
2016-07-25 00:10:13 -0400

    Remove old templates and urls

--- sts/explorer/urls.py
+++ sts/explorer/urls.py
@@ -6,9 +6,4 @@ urlpatterns = [
     url(r'^beaconapi/$', views.beaconApi, name='beaconApi'),
     url(r'^modalapi/$', views.modalApi, name='modalApi'),
     url(r'^precipapi/$', views.precipApi, name='precipApi'),
-    url(r'^about/$', views.about, name='about'),
-    url(r'^datasources/$', views.datasources, name='datasources'),
-    url(r'^criteriascoring/$', views.criteriascoring, name='criteriascoring'),
-    url(r'^findingssolutions/$', views.findingssolutions, name='findingssolutions'),
-
 ]
