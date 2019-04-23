601f2338e8f2ad579ecd4339273051d61b36bcd7
JD Godchaux <jd@nijel.org>
2019-04-22 22:10:09 -0500

    works on the server

--- sts/explorer/management/commands/import_wq_data_from_wqp.py
+++ sts/explorer/management/commands/import_wq_data_from_wqp.py
@@ -27,7 +27,7 @@ class Command(BaseCommand):
                 # SDparsed = dateutil.parser.parse(row[7])
                 # SDObject = SDparsed.date()
                 # get Beaches object to pass
-                beach_id = row[20].split(';')[1]
+                beach_id = row[20].split(';')
                 print beach_id
                 # beach = Beaches.objects.get(BeachID=row[1])
 
