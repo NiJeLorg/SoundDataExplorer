7aa48bd3f0a0ff728a27492f73088dce1b0ac4d5
JD Godchaux <jd@nijel.org>
2019-07-02 07:01:42 -0500

    update modal front end with 48 hour precip

--- sts/explorer/views.py
+++ sts/explorer/views.py
@@ -137,6 +137,8 @@ def modalApi(request):
 	endDate = request.GET.get("endDate","2100-01-01")
 	beachId = request.GET.get("beachId","")
 	tab = request.GET.get("tab","precip")
+	twentySixteen = datetime.date(2016, 1, 1)
+	midnight = datetime.time(0,0,0)
 
 	#setup for CSV files
 	ts = str(int(time.time()))
@@ -187,48 +189,106 @@ def modalApi(request):
 		NumberOfSamples = len(samples)
 		if NumberOfSamples > 0:
 			for sample in samples:
-				today = sample.StartDate
-				yesterday = sample.StartDate + relativedelta(days=-1)
-				twoDaysAgo = sample.StartDate + relativedelta(days=-2)
-				oneYearAgo = sample.StartDate + relativedelta(years=-1)
+				if sample.StartDate < twentySixteen:
+					yesterday = sample.StartDate + relativedelta(days=-1)
+					twoDaysAgo = sample.StartDate + relativedelta(days=-2)
+					oneYearAgo = sample.StartDate + relativedelta(years=-1)
 
-				# check to see if personal weather station data exist for this beach on these days
-				pwscount = WeatherDataPWS.objects.filter(Station__BeachID__exact=beach, Date__gte=twoDaysAgo, Date__lte=yesterday).count()
-				if pwscount > 0:
-					# get the nearest staion with data
-					stations = WeatherStationsPWS.objects.filter(BeachID__exact=beach).order_by('DistanceKm')
-					used_stations = 0
-					for station in stations:
+					# check to see if personal weather station data exist for this beach on these days
+					pwscount = WeatherDataPWS.objects.filter(Station__BeachID__exact=beach, Date__gte=twoDaysAgo, Date__lte=yesterday).count()
+					if pwscount > 0:
+						# get the nearest staion with data
+						stations = WeatherStationsPWS.objects.filter(BeachID__exact=beach).order_by('DistanceKm')
+						used_stations = 0
+						for station in stations:
 
-						# check to see if the station has 0 precipitation within the last year -- if so skip this station
-						precip_check = WeatherDataPWS.objects.filter(Station__exact=station, Date__gte=oneYearAgo, Date__lte=today).aggregate(Sum('PrecipitationIn'))
-						# get count of precip objects excluding any that have a daily value greater than 5 inches and skip if there are none
-						precipcount_under5 = WeatherDataPWS.objects.filter(Station__exact=station, Date__gte=twoDaysAgo, Date__lte=yesterday).exclude(PrecipitationIn__gt=5).count()
+							# check to see if the station has 0 precipitation within the last year -- if so skip this station
+							precip_check = WeatherDataPWS.objects.filter(Station__exact=station, Date__gte=oneYearAgo, Date__lte=yesterday).aggregate(Sum('PrecipitationIn'))
+							# get count of precip objects excluding any that have a daily value greater than 5 inches and skip if there are none
+							precipcount_under5 = WeatherDataPWS.objects.filter(Station__exact=station, Date__gte=twoDaysAgo, Date__lte=yesterday).exclude(PrecipitationIn__gt=5).count()
 
-						if precip_check['PrecipitationIn__sum'] > 0 and precipcount_under5 > 0:
-							# pull precip data for next step
-							precip = WeatherDataPWS.objects.filter(Station__exact=station, Date__gte=twoDaysAgo, Date__lte=yesterday).aggregate(Sum('PrecipitationIn'))
-							#if there are precip objects and the sum of precip is > 0, then break the for loop
-							if precip['PrecipitationIn__sum'] is not None:
+							if precip_check['PrecipitationIn__sum'] > 0 and precipcount_under5 > 0:
+								# pull precip data for next step
+								precip = WeatherDataPWS.objects.filter(Station__exact=station, Date__gte=twoDaysAgo, Date__lte=yesterday).aggregate(Sum('PrecipitationIn'))
 								used_stations += 1
-								stationCode = station
 								break
 
-					if used_stations == 0:
-						# fall back to the airport precip data if no personal weather stations with usable data
+						if used_stations == 0:
+							# fall back to the airport precip data if no personal weather stations with usable data
+							precip = WeatherData.objects.filter(Station__BeachID__exact=beach, Date__gte=twoDaysAgo, Date__lte=yesterday).aggregate(Sum('PrecipitationIn'))
+
+					else:
+						# fall back to the airport precip data if no personal weather stations nearby
 						precip = WeatherData.objects.filter(Station__BeachID__exact=beach, Date__gte=twoDaysAgo, Date__lte=yesterday).aggregate(Sum('PrecipitationIn'))
-						# look up Weather Station 
-						ws = WeatherStations.objects.get(BeachID__exact=beach)
-						stationCode = ws.Icao
+
+					fortyEightHourPrecip = precip['PrecipitationIn__sum']
 
 				else:
-					# fall back to the airport precip data if no personal weather stations nearby
-					precip = WeatherData.objects.filter(Station__BeachID__exact=beach, Date__gte=twoDaysAgo, Date__lte=yesterday).aggregate(Sum('PrecipitationIn'))
-					# look up Weather Station 
-					ws = WeatherStations.objects.get(BeachID__exact=beach)
-					stationCode = ws.Icao
+					# make a date time object from the sample date time in UTC 
+					# if time == '00:00:00' then assume sample is taken at 11am Eastern (6am UTC)
+					if sample.StartTime == midnight:
+						sampleDateTime = datetime.combine(sample.StartDate, time(6,0,0))
+					else:
+						sampleDateTime = datetime.combine(sample.StartDate, sample.StartTime) + relativedelta(hours=-5)
 
-				sample.precipSum = precip['PrecipitationIn__sum']
+					fortyEightHoursAgo = sampleDateTime + relativedelta(hours=-48)
+					oneYearAgo = sampleDateTime + relativedelta(years=-1)
+
+					# times used for selecting precip data below
+					fiftyNineMinutesAgo = sampleDateTime + relativedelta(minutes=-59)
+					twentyFourHoursAgo = sampleDateTime + relativedelta(hours=-24)
+					twentyFiveHoursAgo = twentyFourHoursAgo + relativedelta(minutes=-59)
+
+					# check to see if personal weather station data exist for this beach on these days
+					pwscount = HourlyWeatherDataPWS.objects.filter(Station__BeachID__exact=beach, DateTimeUTC__gte=fortyEightHoursAgo, DateTimeUTC__lte=sampleDateTime).count()
+					if pwscount > 0:
+						# get the nearest staion with data
+						stations = WeatherStationsPWS.objects.filter(BeachID__exact=beach).order_by('DistanceKm')
+						used_stations = 0
+						for station in stations:
+
+							# check to see if the station has 0 precipitation within the last year -- if so skip this station
+							precip_check = HourlyWeatherDataPWS.objects.filter(Station__exact=station, DateTimeUTC__gte=oneYearAgo, DateTimeUTC__lte=sampleDateTime).aggregate(Sum('PrecipitationIn'))
+							# get count of precip objects excluding any that have a daily value greater than 5 inches and skip if there are none
+							precipcount_under5 = HourlyWeatherDataPWS.objects.filter(Station__exact=station, DateTimeUTC__gte=fortyEightHoursAgo, DateTimeUTC__lte=sampleDateTime).exclude(PrecipitationIn__gt=5).count()
+
+							if precip_check['PrecipitationIn__sum'] > 0 and precipcount_under5 > 0:
+								# pull precip data for two time points -- right prior to the sample and 24 hours prior, then sum to get 48 hour cumulative
+								precip_sampleDateTime = HourlyWeatherDataPWS.objects.filter(Station__exact=station, DateTimeUTC__gte=fiftyNineMinutesAgo, DateTimeUTC__lte=sampleDateTime).aggregate(Sum('PrecipitationIn'))
+								
+								precip_twentyFourHoursAgo = HourlyWeatherDataPWS.objects.filter(Station__exact=station, DateTimeUTC__gte=twentyFiveHoursAgo, DateTimeUTC__lte=twentyFourHoursAgo).aggregate(Sum('PrecipitationIn'))
+
+								if precip_sampleDateTime['PrecipitationIn__sum'] is not None and precip_twentyFourHoursAgo['PrecipitationIn__sum'] is not None:
+									fortyEightHourPrecip = precip_sampleDateTime['PrecipitationIn__sum'] + precip_twentyFourHoursAgo['PrecipitationIn__sum']
+									
+									used_stations += 1
+									break
+
+						if used_stations == 0:
+							# fall back to the airport precip data if no personal weather stations with usable data
+							precip_sampleDateTime = HourlyWeatherData.objects.filter(Station__BeachID__exact=beach, DateTimeUTC__gte=fiftyNineMinutesAgo, DateTimeUTC__lte=sampleDateTime).aggregate(Sum('PrecipitationIn'))
+
+							precip_twentyFourHoursAgo = HourlyWeatherData.objects.filter(Station__BeachID__exact=beach, DateTimeUTC__gte=twentyFiveHoursAgo, DateTimeUTC__lte=twentyFourHoursAgo).aggregate(Sum('PrecipitationIn'))
+
+							if precip_sampleDateTime['PrecipitationIn__sum'] is not None and precip_twentyFourHoursAgo['PrecipitationIn__sum'] is not None:
+								fortyEightHourPrecip = precip_sampleDateTime['PrecipitationIn__sum'] + precip_twentyFourHoursAgo['PrecipitationIn__sum']
+							else:
+								fortyEightHourPrecip = 0										
+
+
+					else:
+						# fall back to the airport precip data if no personal weather stations nearby
+						precip_sampleDateTime = HourlyWeatherData.objects.filter(Station__BeachID__exact=beach, DateTimeUTC__gte=fiftyNineMinutesAgo, DateTimeUTC__lte=sampleDateTime).aggregate(Sum('PrecipitationIn'))
+
+						precip_twentyFourHoursAgo = HourlyWeatherData.objects.filter(Station__BeachID__exact=beach, DateTimeUTC__gte=twentyFiveHoursAgo, DateTimeUTC__lte=twentyFourHoursAgo).aggregate(Sum('PrecipitationIn'))
+
+						if precip_sampleDateTime['PrecipitationIn__sum'] is not None and precip_twentyFourHoursAgo['PrecipitationIn__sum'] is not None:
+							fortyEightHourPrecip = precip_sampleDateTime['PrecipitationIn__sum'] + precip_twentyFourHoursAgo['PrecipitationIn__sum']
+						else:
+							fortyEightHourPrecip = 0
+
+
+				sample.precipSum = fortyEightHourPrecip
 
 				# make list of sample values for gmean
 				# exclude Fecal Coliform from geomean
@@ -271,50 +331,106 @@ def modalApi(request):
 		NumberOfSamples = len(allSamples)
 		if NumberOfSamples > 0:
 			for sample in allSamples:
-				today = sample.StartDate
-				yesterday = sample.StartDate + relativedelta(days=-1)
-				twoDaysAgo = sample.StartDate + relativedelta(days=-2)
-				oneYearAgo = sample.StartDate + relativedelta(years=-1)
+				if sample.StartDate < twentySixteen:
+					yesterday = sample.StartDate + relativedelta(days=-1)
+					twoDaysAgo = sample.StartDate + relativedelta(days=-2)
+					oneYearAgo = sample.StartDate + relativedelta(years=-1)
 
-				# check to see if personal weather station data exist for this beach on these days
-				pwscount = WeatherDataPWS.objects.filter(Station__BeachID__exact=beach, Date__gte=twoDaysAgo, Date__lte=yesterday).count()
-				if pwscount > 0:
-					# get the nearest staion with data
-					stations = WeatherStationsPWS.objects.filter(BeachID__exact=beach).order_by('DistanceKm')
-					used_stations = 0
-					for station in stations:
+					# check to see if personal weather station data exist for this beach on these days
+					pwscount = WeatherDataPWS.objects.filter(Station__BeachID__exact=beach, Date__gte=twoDaysAgo, Date__lte=yesterday).count()
+					if pwscount > 0:
+						# get the nearest staion with data
+						stations = WeatherStationsPWS.objects.filter(BeachID__exact=beach).order_by('DistanceKm')
+						used_stations = 0
+						for station in stations:
 
-						# check to see if the station has 0 precipitation within the last year -- if so skip this station
-						precip_check = WeatherDataPWS.objects.filter(Station__exact=station, Date__gte=oneYearAgo, Date__lte=today).aggregate(Sum('PrecipitationIn'))
-						# get count of precip objects excluding any that have a daily value greater than 5 inches and skip if there are none
-						precipcount_under5 = WeatherDataPWS.objects.filter(Station__exact=station, Date__gte=twoDaysAgo, Date__lte=yesterday).exclude(PrecipitationIn__gt=5).count()
+							# check to see if the station has 0 precipitation within the last year -- if so skip this station
+							precip_check = WeatherDataPWS.objects.filter(Station__exact=station, Date__gte=oneYearAgo, Date__lte=yesterday).aggregate(Sum('PrecipitationIn'))
+							# get count of precip objects excluding any that have a daily value greater than 5 inches and skip if there are none
+							precipcount_under5 = WeatherDataPWS.objects.filter(Station__exact=station, Date__gte=twoDaysAgo, Date__lte=yesterday).exclude(PrecipitationIn__gt=5).count()
 
-						if precip_check['PrecipitationIn__sum'] > 0 and precipcount_under5 > 0:
-							# pull precip data for next step
-							precip = WeatherDataPWS.objects.filter(Station__exact=station, Date__gte=twoDaysAgo, Date__lte=yesterday).aggregate(Sum('PrecipitationIn'))
-							#if there are precip objects and the sum of precip is > 0, then break the for loop
-							if precip['PrecipitationIn__sum'] is not None:
+							if precip_check['PrecipitationIn__sum'] > 0 and precipcount_under5 > 0:
+								# pull precip data for next step
+								precip = WeatherDataPWS.objects.filter(Station__exact=station, Date__gte=twoDaysAgo, Date__lte=yesterday).aggregate(Sum('PrecipitationIn'))
 								used_stations += 1
-								stationCode = station
 								break
 
-					if used_stations == 0:
-						# fall back to the airport precip data if no personal weather stations with usable data
-						precip = WeatherData.objects.filter(Station__BeachID__exact=beach, Date__gte=twoDaysAgo, Date__lte=yesterday).aggregate(Sum('PrecipitationIn'))
-						# look up Weather Station 
-						ws = WeatherStations.objects.get(BeachID__exact=beach)
-						stationCode = ws.Icao
+						if used_stations == 0:
+							# fall back to the airport precip data if no personal weather stations with usable data
+							precip = WeatherData.objects.filter(Station__BeachID__exact=beach, Date__gte=twoDaysAgo, Date__lte=yesterday).aggregate(Sum('PrecipitationIn'))
 
+					else:
+						# fall back to the airport precip data if no personal weather stations nearby
+						precip = WeatherData.objects.filter(Station__BeachID__exact=beach, Date__gte=twoDaysAgo, Date__lte=yesterday).aggregate(Sum('PrecipitationIn'))
+
+					fortyEightHourPrecip = precip['PrecipitationIn__sum']
 
 				else:
-					# fall back to the airport precip data if no personal weather stations nearby
-					precip = WeatherData.objects.filter(Station__BeachID__exact=beach, Date__gte=twoDaysAgo, Date__lte=yesterday).aggregate(Sum('PrecipitationIn'))
-					# look up Weather Station 
-					ws = WeatherStations.objects.get(BeachID__exact=beach)
-					stationCode = ws.Icao
+					# make a date time object from the sample date time in UTC 
+					# if time == '00:00:00' then assume sample is taken at 11am Eastern (6am UTC)
+					if sample.StartTime == midnight:
+						sampleDateTime = datetime.combine(sample.StartDate, time(6,0,0))
+					else:
+						sampleDateTime = datetime.combine(sample.StartDate, sample.StartTime) + relativedelta(hours=-5)
 
-				sample.precipSum = precip['PrecipitationIn__sum']
+					fortyEightHoursAgo = sampleDateTime + relativedelta(hours=-48)
+					oneYearAgo = sampleDateTime + relativedelta(years=-1)
 
+					# times used for selecting precip data below
+					fiftyNineMinutesAgo = sampleDateTime + relativedelta(minutes=-59)
+					twentyFourHoursAgo = sampleDateTime + relativedelta(hours=-24)
+					twentyFiveHoursAgo = twentyFourHoursAgo + relativedelta(minutes=-59)
+
+					# check to see if personal weather station data exist for this beach on these days
+					pwscount = HourlyWeatherDataPWS.objects.filter(Station__BeachID__exact=beach, DateTimeUTC__gte=fortyEightHoursAgo, DateTimeUTC__lte=sampleDateTime).count()
+					if pwscount > 0:
+						# get the nearest staion with data
+						stations = WeatherStationsPWS.objects.filter(BeachID__exact=beach).order_by('DistanceKm')
+						used_stations = 0
+						for station in stations:
+
+							# check to see if the station has 0 precipitation within the last year -- if so skip this station
+							precip_check = HourlyWeatherDataPWS.objects.filter(Station__exact=station, DateTimeUTC__gte=oneYearAgo, DateTimeUTC__lte=sampleDateTime).aggregate(Sum('PrecipitationIn'))
+							# get count of precip objects excluding any that have a daily value greater than 5 inches and skip if there are none
+							precipcount_under5 = HourlyWeatherDataPWS.objects.filter(Station__exact=station, DateTimeUTC__gte=fortyEightHoursAgo, DateTimeUTC__lte=sampleDateTime).exclude(PrecipitationIn__gt=5).count()
+
+							if precip_check['PrecipitationIn__sum'] > 0 and precipcount_under5 > 0:
+								# pull precip data for two time points -- right prior to the sample and 24 hours prior, then sum to get 48 hour cumulative
+								precip_sampleDateTime = HourlyWeatherDataPWS.objects.filter(Station__exact=station, DateTimeUTC__gte=fiftyNineMinutesAgo, DateTimeUTC__lte=sampleDateTime).aggregate(Sum('PrecipitationIn'))
+								
+								precip_twentyFourHoursAgo = HourlyWeatherDataPWS.objects.filter(Station__exact=station, DateTimeUTC__gte=twentyFiveHoursAgo, DateTimeUTC__lte=twentyFourHoursAgo).aggregate(Sum('PrecipitationIn'))
+
+								if precip_sampleDateTime['PrecipitationIn__sum'] is not None and precip_twentyFourHoursAgo['PrecipitationIn__sum'] is not None:
+									fortyEightHourPrecip = precip_sampleDateTime['PrecipitationIn__sum'] + precip_twentyFourHoursAgo['PrecipitationIn__sum']
+									
+									used_stations += 1
+									break
+
+						if used_stations == 0:
+							# fall back to the airport precip data if no personal weather stations with usable data
+							precip_sampleDateTime = HourlyWeatherData.objects.filter(Station__BeachID__exact=beach, DateTimeUTC__gte=fiftyNineMinutesAgo, DateTimeUTC__lte=sampleDateTime).aggregate(Sum('PrecipitationIn'))
+
+							precip_twentyFourHoursAgo = HourlyWeatherData.objects.filter(Station__BeachID__exact=beach, DateTimeUTC__gte=twentyFiveHoursAgo, DateTimeUTC__lte=twentyFourHoursAgo).aggregate(Sum('PrecipitationIn'))
+
+							if precip_sampleDateTime['PrecipitationIn__sum'] is not None and precip_twentyFourHoursAgo['PrecipitationIn__sum'] is not None:
+								fortyEightHourPrecip = precip_sampleDateTime['PrecipitationIn__sum'] + precip_twentyFourHoursAgo['PrecipitationIn__sum']
+							else:
+								fortyEightHourPrecip = 0										
+
+
+					else:
+						# fall back to the airport precip data if no personal weather stations nearby
+						precip_sampleDateTime = HourlyWeatherData.objects.filter(Station__BeachID__exact=beach, DateTimeUTC__gte=fiftyNineMinutesAgo, DateTimeUTC__lte=sampleDateTime).aggregate(Sum('PrecipitationIn'))
+
+						precip_twentyFourHoursAgo = HourlyWeatherData.objects.filter(Station__BeachID__exact=beach, DateTimeUTC__gte=twentyFiveHoursAgo, DateTimeUTC__lte=twentyFourHoursAgo).aggregate(Sum('PrecipitationIn'))
+
+						if precip_sampleDateTime['PrecipitationIn__sum'] is not None and precip_twentyFourHoursAgo['PrecipitationIn__sum'] is not None:
+							fortyEightHourPrecip = precip_sampleDateTime['PrecipitationIn__sum'] + precip_twentyFourHoursAgo['PrecipitationIn__sum']
+						else:
+							fortyEightHourPrecip = 0
+
+
+				sample.precipSum = fortyEightHourPrecip
 
 				# add to CSV
 				#empty list for a row
@@ -344,7 +460,6 @@ def modalApi(request):
 	sampleAggregates = BeachWQSamples.objects.filter(StartDate__range=[startDateobject,endDateobject],BeachID__exact=beach).exclude(CharacteristicName__exact="Total Coliform").values('BeachID').annotate(AvgValue=Avg('ResultValue'),MinValue=Min('ResultValue'),MaxValue=Max('ResultValue'))
 
 	#calculate the geometric mean
-	print (sampleList)
 	geomean = gmean(sampleList)
 
 	return render(request, 'explorer/modal.html', {'startDate': startDateobject, 'endDate': endDateobject, 'beach':beach , 'tab':tab ,'scores': scores, 'samples': samples, 'latestSample': latestSample, 'earliestSample': earliestSample, 'sampleAggregates':sampleAggregates, 'geomean':geomean, 'folder':folder, 'filename_all':filename_all, 'filename_filtered':filename_filtered, 'beachStory': story['url']})
